260adab35256fad0b7eb9b527723c2cf2f900b03
Isale <isale.bryan@gmail.com>
2020-07-01 09:31:42 +0300

    Model for book instance

---
+++ models/bookinstance.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var BookInstanceSchema = new Schema({
+book: {
+  type: Schema.Types.ObjectId, 
+  ref: 'Book',
+  required: true
+},
+imprint: {
+  type: String,
+  required: true
+},
+status: {
+  type: String,
+  required: true,
+  enum: ['Available','Maintenance', 'Loaned', 'Reserved']
+},
+due_back: {
+  type: Date,
+  default: Date.now
+}
+});
+
+//Virtual for bookinstance's URL
+BookInstanceSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/bookinstance/' + this._id;
+});
+
+//Export model 
+module.exports = mongoose.model('BookInstance', BookInstanceSchema);
