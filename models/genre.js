285e21115685e2c1aaf09c9d108da600c672e715
Isale <isale.bryan@gmail.com>
2020-07-01 09:37:43 +0300

    genre schema

---
+++ models/genre.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var GenreSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength:3,
+    maxlength: 100
+  }
+});
+
+// virtuals for genre schema
+GenreSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/genre/' + this._id;
+});
+
+// Export Model
+module.exports = mongoose.model('Genre', GenreSchema);
