f25672e8afa3133ab560da99fcd1d9e0d3e1c077
Isale <isale.bryan@gmail.com>
2020-07-01 09:23:22 +0300

    book model

--- models/book.js
+++ models/book.js
@@ -1,1 +1,33 @@
 var mongoose = require('mongoose');
+const { schema } = require('./author');
+var Schema = mongoose.Schema;
+
+var BookSchema = new Schema({
+    title: {
+      type:String, 
+      required: true
+    },
+    author: {
+      type: Schema.Types.ObjectId, ref: 'Author', required: true
+    },
+    summary: {
+      type: String,
+       required: true
+      },
+    isbn: {
+      type: String, 
+      required: true
+    },
+    genre:[{
+      type: schema.Types.ObjectId, ref: 'Genre'
+    }]
+  });
+
+  //Virtual for book's URL
+  BookSchema.virtual('url')
+  .get(function () {
+    return '/catalog/book/' + this._id;
+  });
+
+  //Export model
+  module.exports = mongoose.model('Book', BookSchema);
