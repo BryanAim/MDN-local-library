7d52d9b4ffd9c8100f6642bdb1deadf9e8bef795
Isale <isale.bryan@gmail.com>
2020-07-02 15:37:06 +0300

    get author list and render

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,9 +1,22 @@
+// TODO
+// Note: The appearance of the author lifespan dates is ugly! You can improve this using the same approach as we used for the BookInstance list (adding the virtual property for the lifespan to the Author model). This time, however, there are missing dates, and references to nonexistent properties are ignored unless strict mode is in effect. moment() returns the current time, and you don't want missing dates to be formatted as if they were today. One way to deal with this is to define the body of the function that returns a formatted date so it returns a blank string unless the date actually exists. For example:
+
+// return this.date_of_birth ? moment(this.date_of_birth).format('YYYY-MM-DD') : '';
+
+
 var Author = require('../models/author');
 
 // Display list of all Authors
 
-exports.author_list = function(req, res) {
-  res.send('NOT IMPLEMENTED:Author list')
+exports.author_list = function(req, res, next) {
+  Author.find()
+  .populate('author')
+  .sort([['last_name', 'ascending']])
+  .exec(function (err, list_authors) {
+    if(err) { return next(err); }
+    //Successful, so render
+    res.render('author_list', { title: 'Author List', author_list: list_authors});
+  });
 };
 
 //Display detail page for a specific author
