34dd14c90938a8b55c259ac2dd6db25203a36aa6
Isale <isale.bryan@gmail.com>
2020-07-06 12:46:10 +0300

    big fixes

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -206,12 +206,12 @@ exports.book_update_get = function(req, res, next) {
     }
     // Success.
     //Mark our selected genres as checked.
-    for (let allGenreIteration = 0; allGenreIteration < results.genre.length; allGenreIteration++) {
-      for(let bookGenreIteration = 0; allGenreIteration < results.book.genre.length; bookGenreIteration++) {
-      if (results.genres[allGenreIteration]._id.toString()==results.book.genre[bookGenreIteration]._id.toString()) {
-        results.genres[allGenreIteration].checked='true'
+    for (var all_g_iter = 0; all_g_iter < results.genres.length; all_g_iter++) {
+      for (var book_g_iter = 0; book_g_iter < results.book.genre.length; book_g_iter++) {
+          if (results.genres[all_g_iter]._id.toString()==results.book.genre[book_g_iter]._id.toString()) {
+              results.genres[all_g_iter].checked='true';
+          }
       }
-    }    
   }
   res.render('book_form', { title: 'Update Book', authors: results.authors, genres: results.genres, book: results.book });
   });
