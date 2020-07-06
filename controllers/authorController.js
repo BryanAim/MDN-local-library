acc1b17f5a99b09f525abfbf23138329f736cdda
Isale <isale.bryan@gmail.com>
2020-07-06 11:16:47 +0300

    Display Author delete form on GET

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -114,9 +114,27 @@ exports.author_create_post = [
 
 ]
 
-// Display Author delete on GET
-exports.author_delete_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: Author delete get');
+// Display Author delete form on GET
+exports.author_delete_get = function (req, res, next) {
+  
+  async.parallel({
+    author: function(callback) {
+      Author.findById(req.params.id).exec(callback)
+    },
+    authors_books: function (callback) {
+      Book.find({'author': req.params.id }).exec(callback)
+    },
+  }, function (err, results) {
+    if (err) {
+      return next(err)
+    }
+    if (results.author==null) {
+      //No results
+      res.redirect('/catalog/authors');
+    }
+    //Successful, so render
+    re.render('author_delete', { title: 'Delete Author', author: results.author, author_books: results.authord_books });
+  });
 };
 
 
