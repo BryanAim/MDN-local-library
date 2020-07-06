e5c3777f6405be846ae90a116bdd9e8095cd0416
Isale <isale.bryan@gmail.com>
2020-07-06 12:57:31 +0300

    compression library for compressing all routes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog')//Import routes for "catalog" area of site
+var compression = require('compression');
 require('dotenv').config();
 
 var app = express();
@@ -26,6 +27,9 @@ db.on('error', console.error.bind(console, 'MongoDB connection Error: '));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+// Compress all routes
+app.use(compression());
 
 app.use(logger('dev'));
 app.use(express.json());
