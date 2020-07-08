e4747855b0f3a073472de477f43931a04aad8b49
Isale <isale.bryan@gmail.com>
2020-07-08 11:23:08 +0300

    Implement Display Author update form on GET function

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -169,8 +169,19 @@ exports.author_delete_post = function (req, res, next) {
 };
 
 //Display Author update form on GET
-exports.author_update_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: Author update get');
+exports.author_update_get = function (req, res, next) {
+  
+  Author.findById(req.params.id, function(err, author) {
+    if (err) { return next(err)}
+    if (author==null) {
+      //No results
+      var err = new Error('Author not found');
+      err.status=404;
+      return next(err);
+    }
+    // Success
+    res.render('author_form', {title: 'Update Author', author: author});
+  })
 };
 
 
