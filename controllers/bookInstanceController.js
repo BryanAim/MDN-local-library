bbf4740a3eb89b1cb5855fee14a827877547a2c3
Isale <isale.bryan@gmail.com>
2020-07-08 10:23:34 +0300

    Handle book update on POST function

--- controllers/bookInstanceController.js
+++ controllers/bookInstanceController.js
@@ -175,7 +175,83 @@ exports.bookinstance_update_get = function (req, res, next) {
   })
 };
 
-// Display BookInstance update on POST
-exports.bookinstance_update_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: Book instance update post');
-};
+// Handle BookInstance update on POST
+exports.bookinstance_update_post = [
+
+  //Convert the genre to an array
+  (req, res, next)=> {
+    if (!(req.body.genre instanceof Array)) {
+      if (typeof req.body.genre=='undefined') {
+        req.body.genre = [];
+      } else {
+        req.body.genre = new Array(req.body.genre);
+      }
+    }
+    next();
+  },
+
+  // Validate fields
+  body('title', 'Title must not be empty').isLength({ min: 1}).trim(),
+  body('author', 'Author must not be empty.').isLength({ min: 1}).trim(),
+  body('summary', 'summary must not be empty').isLength({ min: 1}).trim(),
+  body('isbn', 'ISBN must not be empty.').isLength({ min: 1}).trim(),
+
+  //sanitize fields
+  sanitizeBody('title').escape(),
+  sanitizeBody('author').escape(),
+  sanitizeBody('summary').escape(),
+  sanitizeBody('isbn').escape(),
+  sanitizeBody('genre.*').escape(),
+  
+  // Process request after validation and sanitization
+  (req, res, next) => {
+
+    // Extract the validation errors from a request
+    const errors = validationResult(req)
+
+    //Create a book object with escaped/ trimmed data
+
+    var book = new Book({
+      title: req.body.title,
+      author: req.body.author,
+      summary: req.body.summary,
+      isbn: req.body.isbn,
+      genre: (typeof req.body.genre=='undefined') ? [] : req.body.genre,
+      _id: req.params.id //REQUIRED , or a new ID wii be assigned
+    });
+
+    if (!errors.isEmpty()) {
+      //No errors, render form again with sanitized values/ error messages
+
+      //get all authors and genres for form
+      async.parallel({
+        authors: function(callback) {
+          Author.find(callback)
+        },
+        genres: function (callback) {
+          Genre.find(callback);
+        },
+      }, function (err, results) {
+        if (err) { return next(err) }
+
+        //Mark our selected genres as checkedbundleRenderer.renderToStream
+        for (let i = 0; i < results.genres.length; i++) {
+          if (book.genre.indexOf(results.genres[i]._id) > -1) {
+            results.genres[i].checked='true';
+          }
+          
+        }
+        res.render('book_form', { title: 'Update book', authors: results.authors, genres: results.genres, book: book, errors: errors.array()})
+      });
+      return;
+    } else {
+      //Data from form is valid. Update the record
+      Book.findByIdAndUpdate(req.params.id, book, {}, function (err, thebook) {
+        if (err) { return next(err) 
+        }
+        //Successful - redirect to book detail page
+        res.redirect(thebook.url);
+      })
+    }
+  }
+]
