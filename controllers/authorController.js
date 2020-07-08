414535b2f8163c65d897e1077505d4b74c4baf3f
Isale <isale.bryan@gmail.com>
2020-07-08 11:40:15 +0300

    implement Handle Author update on POST function

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -186,6 +186,62 @@ exports.author_update_get = function (req, res, next) {
 
 
 // Handle Author update on POST
-exports.author_update_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: Author update post');
-};
+exports.author_update_post = [
+
+  // Validate fields
+  body('first_name')
+  .isLength({ min: 1})
+  .trim()
+  .withMessage('First name must be specified')
+  .isAlphanumeric().withMessage('First name has non-alphanumeric characters'),
+
+  body('last_name')
+  .isLength({ min: 1})
+  .trim()
+  .withMessage('Last name must be specified')
+  .isAlphanumeric().withMessage('Last name has non-alphanumeric characters'),
+  
+  body('date_of_birth', 'Invalid date of birth')
+  .optional({ checkFalsy: true})
+  .isISO8601(),
+  body('date_of_death', 'Invalid date of death')
+  .optional({ checkFalsy: true})
+  .isISO8601(),
+
+  //Sanitize fields
+  sanitizeBody('first_name').escape(),
+  sanitizeBody('last_name').escape(),
+  sanitizeBody('date_of_birth').toDate(),
+  sanitizeBody('date_of_death').toDate(),
+
+  // Process request after validation and sanitization
+  (req, res, next) => {
+
+    //Extract  the validation errors from a request 
+    const errors = validationResult(req);
+
+    // Create Author object with escaped and trimmed data (and the old id)
+
+    var author = new Author({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body,date_of_death,
+      _id: req.params.id
+    });
+
+    if (!errors.isEmpty()) {
+      // No errors, render the form again with sanitized values and error messages
+      res.render('author_form', { title: 'Update Author', author: author, errors: errors.array()});
+      return;
+    } else {
+      
+      //Data from form is valid, update the record
+      Author.findByIdAndUpdate(req.params.id, author, {}, function (err, theauthor) {
+        if(err) { return next(err) }
+        // successful -  redirect to author detail page
+        res.redirect(theauthor.url)
+      })
+    }
+  }
+]
